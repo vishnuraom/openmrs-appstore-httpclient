736b7314c470fc84ae809d94f157f2b3fa313674
vishnuraom <vishnumechineni@gmail.com>
2016-06-13 19:23:27 +0530

    Generating url based on request

--- routes/packages.js
+++ routes/packages.js
@@ -3,13 +3,14 @@ var request = require('request');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-       request.get({ url: "https://api.bintray.com/repos/vishnuraom/omod/packages"},
-       function(error, response, body) {
-              if (!error && response.statusCode == 200) {
-                var obj = JSON.parse(body);
-                res.json(obj);
-                }
-              });
+      var repo = req.query['repo'].toLowerCase();
+      request.get({ url: "https://api.bintray.com/repos/vishnuraom/"+repo+"/packages"},
+      function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+              var obj = JSON.parse(body);
+              res.json(obj);
+              }
+            });
 });
 
 module.exports = router;
